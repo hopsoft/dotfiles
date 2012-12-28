f93aad45c5c62c1e4f20ab5ccecc6ce0590bcb43
Nathan Hopkins <natehop@gmail.com>
2012-12-28 14:49:25 -0700

    Simplifying.

--- scripts/dotdir.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-dir = File.expand_path(File.dirname(__FILE__))
-if dir == "/usr/bin"
-  dir = File.expand_path(File.join(File.readlink("/usr/bin/dotdir"), "..", "..")) rescue nil
-else
-  dir = File.expand_path(File.join(dir, ".."))
-end
-puts dir
