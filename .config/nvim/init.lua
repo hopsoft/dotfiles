3e009e333facd6c0a4855d228ee08707935984e0
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-10-24 22:06:50 -0600

    Updates for neovim ChatGPT prompts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,9 @@
-package.path = os.getenv("DOTDIR") .. '/.config/nvim/lua/?.lua;' .. package.path 
+package.path = os.getenv("DOTDIR") .. '/.config/nvim/lua/?.lua;' .. package.path
 require("plugins")
 require("mappings")
 require("settings")
-require("chatgpt")
 require("fzf")
+
+-- require("chatgpt") doesn't work
+-- but it can be loaded successfully via dofile
+dofile(os.getenv("DOTDIR") .. '/.config/nvim/lua/chatgpt.lua')
