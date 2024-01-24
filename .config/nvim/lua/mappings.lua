5ca64eebfd107e9294266a716fd02c54c7591b5b
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-01-24 12:51:20 -0700

    Add ctags for ActiveRecord scopes

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ","
 
 vim.api.nvim_set_keymap('i', 'jk', '<esc>', {})
-vim.api.nvim_set_keymap('n', '<leader>h', ':set hls! hls?<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>h', ':set hls! hls?<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>nf', ':NERDTreeFind<CR>', {})
 vim.api.nvim_set_keymap('n', '<Leader>nt', ':NERDTreeToggle<CR>', {})
 vim.api.nvim_set_keymap('n', '<Leader>tb', ':TagbarToggle<CR>', {})
