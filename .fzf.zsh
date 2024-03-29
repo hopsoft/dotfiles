49d06dba17eef6b9b7f51789ec81f008823b6f74
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-03-29 14:14:13 -0600

    Add fish script and default fzf options

--- .fzf.zsh
+++ .fzf.zsh
@@ -6,7 +6,5 @@ fi
 . "$BREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
 
 export FZF_BASE="$BREW_PREFIX/bin/fzf"
-#export FZF_DEFAULT_COMMAND="fd --hidden --type=file --color=never"
-#export FZF_DEFAULT_OPTS="--ansi --tabstop=2"
-#export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-#export FZF_CTRL_T_OPTS="--preview 'bat --force-colorization --highlight-line 60 {}'"
+export FZF_DEFAULT_COMMAND="fd --hidden --type=file --color=never"
+export FZF_DEFAULT_OPTS="--ansi --color=dark --tabstop=2"
